e0ff980f0a20ca48ed616bda59ad1cea7315d283
Sitora16 <axatqulovasitora@gmail.com>
2025-04-14 16:07:17 +0500

    Create lesson1.sql

---
+++ Lesson-1/homework/lesson1.sql
@@ -0,0 +1,16 @@
+Create database SchoolDB;
+Create table Students(
+StudentID int Primary Key,
+Name varchar(50),
+Age int);
+
+Insert into Students(
+StudentID,
+Name,
+Age 
+)values
+(1,'Ali',20),
+(2,'Vali',21),
+(3,'Karim',22);
+
+Create Database SchoolDB;
